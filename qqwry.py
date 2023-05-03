797cd69deca418fc3eb35575b45c580625a0de5a
FW27623 <1970446279@qq.com>
2023-05-03 18:00:36 +0000

    test -6

--- qqwry.py
+++ qqwry.py
@@ -3,13 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-CN;q=0.8,en;q=0.7,zh-TW;q=0.6',
+    'Cookie': 'rewardsn=; wxtokenkey=777',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
 }
 
 url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg3Mzc0NTA3NA==&action=getalbum&album_id=2329805780276838401#wechat_redirect'
 
+#访问链接加上headers
 response = requests.get(url, headers=headers)
+#解析网页
 soup = BeautifulSoup(response.text, 'html.parser')
+#提取微信推文链接
 link = soup.find('li', {'class': 'album__list-item js_album_item js_wx_tap_highlight wx_tap_cell'}).get('data-link')
 #访问微信推文链接
 response = requests.get(link)
